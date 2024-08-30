5fa435c70de390e17be1e50f9aa14ae38bfad92e
yourgrandmadiscord <77011897+yourgrandmadiscord@users.noreply.github.com>
2024-08-30 15:36:56 +0100

    Create suspend.py

---
+++ suspend/suspend.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.checks import PermissionLevel
+
+
+class SuspendThread(commands.Cog):
+    """Allows supporters to suspend a thread."""
+    def __init__(self, bot):
+        self.bot = bot
+        self.db = self.bot.plugin_db.get_partition(self)
+
+
+    @checks.has_permissions(PermissionLevel.SUPPORTER)
+    @checks.thread_only()
+    @commands.command()
+    async def suspend(self, ctx):
+        thread = await self.db.find_one({'thread_id': str(ctx.thread.channel.id)})
+        if thread:
+            await self.db.delete_one({'thread_id': str(ctx.thread.channel.id)})
+
+async def setup(bot):
+    await bot.add_cog(SuspendThread(bot))
